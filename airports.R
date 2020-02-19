d1e451c228be877b1a6bdc5acd4c0b780ba493a4
ryazadd <d.ryazanova07@gmail.com>
2020-02-19 11:02:21 +0300

    sqldf in r

--- airports.R
+++ airports.R
@@ -15,10 +15,26 @@ names(airportdat)[5] <- "Enplaned_passengers"
 names(airportdat)[6] <- "Tons_freight"
 names(airportdat)[7] <- "Tons_mail"
 
-hist(airportdat$Scheduled_departures, col="red")
-hist(airportdat$Performed_departures,add=T)
+hist(airportdat$Scheduled_departures, col=rgb(225/255,244/255, 243/255)
+     ,main = "Scheduled departments"
+     ,xlab="Numbers of depsrtments")
+
+hist(airportdat$Performed_departures, col=rgb(248/255,180/255,0)
+     , main="Distrubition Performed_departures"
+     ,xlab="Number of passangers")
 
 hist(airportdat$Tons_freight, col="blue")
 hist(airportdat$Tons_mail,add=T, col="red")
 
-boxplot(airportdat$Tons_freight)
+boxplot(airportdat$Tons_freight, main = "Tons_freight")
+boxplot(airportdat$Performed_departures, main = "Performed_departures")
+
+City_Pass<-sqldf("select City, SUM(Enplaned_passengers) as sum from airportdat group by City")
+Max_city_Pas<-sqldf("select City from City_Pass order by sum desc limit 1")
+
+
+City_Tones_freight<-sqldf("select City, SUM(Tons_freight) as sum from airportdat group by City")
+Max_city_freight<-sqldf("select City from City_Tones_freight order by sum desc limit 1")
+
+Max_city_freight
+
